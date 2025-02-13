62b6e8f5f1051b7c9a3b8ceaf24646d20779a742
Sohrab Alam Ansari <ansari.shohrab80@gmail.com>
2025-02-13 11:19:05 +0530

    Test for add function returns zero if empty numbers

---
+++ src/app/services/string-calculator.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class StringCalculatorService {
+  constructor() {}
+  add(numbers: string): number {
+    if (!numbers) {
+      return 0;
+    }
+    return parseInt(numbers, 10) || 0;
+  }
+}
